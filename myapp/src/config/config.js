62a7dc28cdc53701b5125411fb39e23b96a9959d
externalPointerVariable <vicky986847@gmail.com>
2025-03-12 09:10:03 +0000

    Add dotenv for environment variable management and update config to use process.env

--- myapp/src/config/config.js
+++ myapp/src/config/config.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const config = {
-    personalAccessToken: import.meta.env.VITE_GITHUB_TOKEN,
-    emailjsServiceId: import.meta.env.VITE_EMAIL_SERVICE_ID,
-    emailjsTemplateId: import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-    emailjsPublicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
-}
+    personalAccessToken: process.env.VITE_GITHUB_TOKEN?.replace(/^\"|\"$/g, ''),
+    emailjsServiceId: process.env.VITE_EMAIL_SERVICE_ID?.replace(/^\"|\"$/g, ''),
+    emailjsTemplateId: process.env.VITE_EMAIL_TEMPLATE_ID?.replace(/^\"|\"$/g, ''),
+    emailjsPublicKey: process.env.VITE_EMAIL_PUBLIC_KEY?.replace(/^\"|\"$/g, ''),
+};
+
 console.log(config);
 
 export default config;
